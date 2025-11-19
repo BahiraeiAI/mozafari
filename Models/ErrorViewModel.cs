eca7c1b04e8eb5d8b2f6e97b69d508e2dc45d536
Mohammad <bahiraeimmai@gmail.com>
2025-11-19 21:58:51 +0330

    first commit

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+﻿namespace RoshedTehran.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
+
